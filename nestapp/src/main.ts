f836f87dea15bbf76432c0bad092be8cca365dac
Kent <34937017+KomorebiSaw@users.noreply.github.com>
2023-11-28 17:52:21 +0800

    feat: add swagger, fix: printWidth

--- nestapp/src/main.ts
+++ nestapp/src/main.ts
@@ -2,6 +2,8 @@ import { NestFactory } from '@nestjs/core'
 
 import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify'
 
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger'
+
 import { AppModule } from './app.module'
 
 async function bootstrap() {
@@ -12,7 +14,15 @@ async function bootstrap() {
 
     app.setGlobalPrefix('api')
 
-    await app.listen(5000, () => {
+    const config = new DocumentBuilder()
+        .setTitle('X System')
+        .setDescription('The X system API description')
+        .setVersion('1.0')
+        .build()
+    const document = SwaggerModule.createDocument(app, config)
+    SwaggerModule.setup('docs', app, document)
+
+    await app.listen(5000, '127.0.0.1', () => {
         console.log('api: http://localhost:5000')
     })
 }
